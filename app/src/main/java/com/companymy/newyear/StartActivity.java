6b91519bdbf59eac4b51a8cbc891dbaa96a886dc
Юрий <ruban.yurii.a@gmail.com>
2018-12-28 17:36:05 +0200

    Initial commit

---
+++ app/src/main/java/com/companymy/newyear/StartActivity.java
@@ -0,0 +1,17 @@
+package com.companymy.newyear;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class StartActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start);
+        List<String> list = new ArrayList<>();
+    }
+}
