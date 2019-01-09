a151f98a5873e6ef8b4ca9c829536514007f7d77
Юрий <ruban.yurii.a@gmail.com>
2019-01-09 16:04:52 +0200

    add second activity

--- app/src/main/java/com/companymy/newyear/StartActivity.java
+++ app/src/main/java/com/companymy/newyear/StartActivity.java
@@ -1,19 +1,51 @@
 package com.companymy.newyear;
 
+import android.content.Intent;
+import android.graphics.drawable.ClipDrawable;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
-import java.util.ArrayList;
-import java.util.List;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class StartActivity extends AppCompatActivity {
+
+    private  Typewrite writer;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
-        List<String> list = new ArrayList<>();
-        list.add("new");
-        list.add("new brench");
+        ImageView imageView = (ImageView)findViewById(R.id.image_tree_vertical);
+        imageView.setImageResource(R.drawable.christmas_tree_vertical);
+        TextView textView = findViewById(R.id.textFirst);
+
+        String textWrite = "Happy New Year!!! ";
+
+        Typewrite writer = new Typewrite(textView);
+        writer.animateText(textWrite);
+
+//        final EditText editname = (EditText)findViewById(R.id.editName);
+//        final String nameString = editname.getText().toString();
+
+
+        findViewById(R.id.buttonFirst).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                send();
+            }
+        });
+    }
+
+
+    public void send(){
+        EditText editName = (EditText)findViewById(R.id.editName);
+        String nameString = editName.getText().toString();
+        Intent intent = new Intent(this, CongratulationActivity.class);
+        intent.putExtra("name", nameString);
+        startActivity(intent);
     }
 }
